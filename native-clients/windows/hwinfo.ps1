5ac5b7051221abe6934f0945fe60f6e1961f6d06
usermicrodevices <maxkolosov@inbox.ru>
2023-02-07 14:00:42 +0300

    Update hwinfo.ps1

--- native-clients/windows/hwinfo.ps1
+++ native-clients/windows/hwinfo.ps1
@@ -4,8 +4,10 @@ $api_host = "https://127.0.0.1/api"
 $api_login = "test-user"
 $api_pass = "test-password"
 
+$uuid = Get-WmiObject Win32_ComputerSystemProduct  | Select-Object -ExpandProperty UUID
+
 $computer_info = Get-ComputerInfo | ConvertTo-Json
 
 $response = Invoke-WebRequest -Method POST -Body (@{"username"=$api_login; "password"=$api_pass}|ConvertTo-Json) -Uri $api_host/login -ContentType application/json | ConvertFrom-Json
 
-Invoke-WebRequest -Headers @{Authorization = ('Bearer {0}' -f $response.token)} -Method PATCH -Body (@{"status"="1"; "extinfo"=$computer_info} | ConvertTo-Json) -Uri $api_host/data/devices/3ba46cb5f7f340ffa43e348cb789901a -ContentType application/json
+Invoke-WebRequest -Headers @{Authorization = ('Bearer {0}' -f $response.token)} -Method PATCH -Body (@{"status"="1"; "extinfo"=$computer_info} | ConvertTo-Json) -Uri $api_host/data/devices/$uuid -ContentType application/json
