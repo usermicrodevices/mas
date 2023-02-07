1744fdbadf9f43be94e5ce3b307a64f34f9524fc
usermicrodevices <maxkolosov@inbox.ru>
2023-02-07 22:33:43 +0300

    Update hwinfo.sh and hwinfo.ps1

--- native-clients/windows/hwinfo.ps1
+++ native-clients/windows/hwinfo.ps1
@@ -10,4 +10,13 @@ $computer_info = Get-ComputerInfo | ConvertTo-Json
 
 $response = Invoke-WebRequest -Method POST -Body (@{"username"=$api_login; "password"=$api_pass}|ConvertTo-Json) -Uri $api_host/login -ContentType application/json | ConvertFrom-Json
 
-Invoke-WebRequest -Headers @{Authorization = ('Bearer {0}' -f $response.token)} -Method POST -Body (@{"status"="1"; "extinfo"=$computer_info} | ConvertTo-Json) -Uri $api_host/data/devices/$uuid -ContentType application/json
+try
+{
+$response = Invoke-WebRequest -Headers @{Authorization = ('Bearer {0}' -f $response.token)} -Method PATCH -Body (@{"status"="1"; "extinfo"=$computer_info} | ConvertTo-Json) -Uri $api_host/data/devices/$uuid -ContentType application/json
+$StatusCode = $response.StatusCode
+} catch {
+$StatusCode = $_.Exception.Response.StatusCode.value__
+}
+if ($StatusCode -ne 200){
+Invoke-WebRequest -Headers @{Authorization = ('Bearer {0}' -f $response.token)} -Method POST -Body (@{"id"=$uuid, "status"="1"; "extinfo"=$computer_info} | ConvertTo-Json) -Uri $api_host/data/devices/ -ContentType application/json
+}
