1ed42de1ba37faad6bfd38cbef0d3c829b63b563
usermicrodevices <maxkolosov@inbox.ru>
2023-02-08 17:39:12 +0300

    Update hwinfo.sh

--- native-clients/linux/hwinfo.sh
+++ native-clients/linux/hwinfo.sh
@@ -8,10 +8,10 @@ uuid = $(sudo cat /sys/class/dmi/id/product_uuid)
 
 computer_info = $(lshw -json)
 
-token = $(curl --request POST --url $api_host/login --header 'Content-Type: application/json' --data '{"username":$api_login,"password":$api_pass}' | jq -r '.token')
+token=$(curl -s --request POST --url $api_host/login --header 'Content-Type: application/json' --data '{"username":"'$api_login'","password":"'$api_pass'"}'|jq -r '.token')
 
-response = $(curl -s -o /dev/null -w "%{http_code}" --request PATCH --url $api_host/data/devices/$uuid --header 'Authorization: Bearer $token' --header 'Content-Type: application/json' --data '{"status":1, "extinfo":$computer_info}') 
+response = $(curl -s -o /dev/null -w "%{http_code}" --request PATCH --url $api_host/data/devices/$uuid --header 'Authorization: Bearer $token' --header 'Content-Type: application/json' --data '{"status":1, "extinfo":'$computer_info'}') 
 
 if test $response -ne 200; then
-curl --request POST --url $api_host/data/devices/ --header 'Authorization: Bearer $token' --header 'Content-Type: application/json' --data '{"id":$uuid, "status":1, "extinfo":$computer_info}'
+curl --request POST --url $api_host/data/devices/ --header 'Authorization: Bearer '$token --header 'Content-Type: application/json' --data '{"id":"'$uuid'", "status":1, "extinfo":"'$computer_info'"}'
 fi
