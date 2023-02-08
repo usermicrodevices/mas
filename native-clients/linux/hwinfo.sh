02ea8af6c85bb43fd44b83af8e5bd5377c8a9682
usermicrodevices <maxkolosov@inbox.ru>
2023-02-08 17:42:55 +0300

    Update hwinfo.sh

--- native-clients/linux/hwinfo.sh
+++ native-clients/linux/hwinfo.sh
@@ -10,7 +10,7 @@ computer_info = $(lshw -json)
 
 token=$(curl -s --request POST --url $api_host/login --header 'Content-Type: application/json' --data '{"username":"'$api_login'","password":"'$api_pass'"}'|jq -r '.token')
 
-response = $(curl -s -o /dev/null -w "%{http_code}" --request PATCH --url $api_host/data/devices/$uuid --header 'Authorization: Bearer $token' --header 'Content-Type: application/json' --data '{"status":1, "extinfo":'$computer_info'}') 
+response = $(curl -s -o /dev/null -w "%{http_code}" --request PATCH --url $api_host/data/devices/$uuid --header 'Authorization: Bearer '$token --header 'Content-Type: application/json' --data '{"status":1, "extinfo":"'$computer_info'"}') 
 
 if test $response -ne 200; then
 curl --request POST --url $api_host/data/devices/ --header 'Authorization: Bearer '$token --header 'Content-Type: application/json' --data '{"id":"'$uuid'", "status":1, "extinfo":"'$computer_info'"}'
